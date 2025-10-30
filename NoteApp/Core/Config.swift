71b223ae1d928fd980766123a8e9ce2450325eb8
Bryan Alberto Lemus Meza <141671984+Bryan-Meza@users.noreply.github.com>
2025-10-30 12:14:40 -0600

    Modification Config.swift

--- NoteApp/Core/Config.swift
+++ NoteApp/Core/Config.swift
@@ -12,6 +12,6 @@ import Foundation
  * THIS FILE SHOULD BE IN .gitignore
  */
 struct Config {
-    static let supabaseURL = "https://cwwcrlukkbudkfoumchf.supabase.co"
-    static let supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3d2NybHVra2J1ZGtmb3VtY2hmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE3OTAxMDIsImV4cCI6MjA3NzM2NjEwMn0.7d2aPpkQ-MIIk1XUuKQ8L__tI1o8R5QPITVwttj1UBY"
+    static let supabaseURL = ""
+    static let supabaseKey = ""
 }
