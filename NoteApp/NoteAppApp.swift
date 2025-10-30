5567f318580aaa74cd1238228024d269c1af1e11
Bryan Meza <bryanmeza1414@gmail.com>
2025-10-29 23:14:08 -0600

    Initial Commit

---
+++ NoteApp/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by Bryan Meza on 29/10/25.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
